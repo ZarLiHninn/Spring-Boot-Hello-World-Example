949f45cdb484c0063a0302cc5dd2edf46baf4ad1
Zar Li Hnin <sakurazarli@gmail.com>
2020-07-08 21:22:41 +0630

    first commit

---
+++ src/main/java/com/reytech/demo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.reytech.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
+    @GetMapping("/message")
+    public String message(Model model) {
+        model.addAttribute("message", "This is a custom message");
+        return "message";
+    }
+}
